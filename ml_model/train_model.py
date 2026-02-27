d128b1baf8bc191653d86b8c76eaf937435e78b0
unknown <USER@aabdulkhaleq.darwaemaar.com>
2026-02-27 20:03:35 +0300

    backend minor fixes

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -3,25 +3,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-# Load dataset (NO HEADER)
 data = pd.read_csv("../../datasets/raw/spam.csv", header=None)
 
-# Column mapping
 data.columns = ["label", "message", "extra1", "extra2", "extra3"]
 
-# Use only label + message
 X = data["message"]
 y = data["label"]
 
-# Convert text to numbers
 vectorizer = TfidfVectorizer()
 X_vector = vectorizer.fit_transform(X)
 
-# Train model
 model = LogisticRegression()
 model.fit(X_vector, y)
 
-# Save model
 pickle.dump(model, open("scam_model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
 
